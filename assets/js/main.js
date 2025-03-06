29c73d302582520de8904756d0e7b262828b1119
Muhammad Hisham <mohmadetsh874@gmail.com>
2025-03-06 22:52:00 +0200

    Fix: to menu button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,15 @@ const header = document.querySelector('header');
 const hamburger = document.getElementById('hamburger');
 const discoverMenu = document.getElementById('discover-menu');
 const menu = document.querySelector('article');
+const toMenu = document.getElementById('to-menu');
 
 hamburger.addEventListener('click', () => {
   header.classList.toggle('expand-header')
 });
+
+toMenu.addEventListener('click', () => {
+  location.href = '#menu';
+})
 
 discoverMenu.addEventListener('click', () => {
   menu.classList.add('expand-menu')
